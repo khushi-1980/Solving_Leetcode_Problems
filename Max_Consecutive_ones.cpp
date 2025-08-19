9f7bb673928d7732e1e8ba95b8ee43fd8c599cf9
Khushi Shetty <shettykhushi083@gmail.com>
2025-08-19 21:43:09 +0530

    Create Max_Consecutive_ones.cpp

---
+++ Max_Consecutive_ones.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0, maxCount = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                count++;                  
+                if (count > maxCount)
+                    maxCount = count;   
+            } else {
+                count = 0;              
+            }
+        }
+        
+        return maxCount;
+    }
+};
